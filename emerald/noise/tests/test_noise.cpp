bfbe260b89dea3e6bc14374905f4e2f8efcd98e7
Christopher Horvath <blackencino@gmail.com>
2021-02-23 13:41:10 -0800

    WIP on emld core merge

--- emerald/noise/tests/test_noise.cpp
+++ emerald/noise/tests/test_noise.cpp
@@ -1,28 +1,29 @@
 #include <emerald/noise/simplex_noise.h>
+#include <emerald/util/format.h>
 #include <emerald/util/random.h>
 
-#include <iostream>
+#include <fmt/format.h>
+#include <gtest/gtest.h>
 
-using namespace emerald::noise;
+#include <cstdint>
 
-//-*****************************************************************************
-void noiseTest1() {
+namespace emerald::noise {
+
+TEST(Noise_test, SimplexNoised) {
     emerald::util::UniformRand gen{0.0, 1.0};
     SimplexNoised snd;
 
     for (int i = 0; i < 100; ++i) {
-        V3d pt(300.0 * (gen() - 0.5),
-               300.0 * (gen() - 0.5),
-               300.0 * (gen() - 0.5));
+        V3d pt(
+          300.0 * (gen() - 0.5), 300.0 * (gen() - 0.5), 300.0 * (gen() - 0.5));
 
         double t = snd(pt);
 
-        std::cout << "Point: " << pt << ", noise val: " << t << std::endl;
+        EXPECT_LT(-2.0, t);
+        EXPECT_GT(2.0, t);
+
+        fmt::print("Point: {}, noise val: {}\n", pt, t);
     }
 }
 
-//-*****************************************************************************
-int main(int, char*[]) {
-    noiseTest1();
-    return 0;
-}
+}  // namespace emerald::noise
